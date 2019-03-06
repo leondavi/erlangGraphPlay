b1fd1f6bf20e9f0f72ed9cd540c0fc2fe38bf7c8
david <ddleon@outlook.co.il>
2019-03-06 23:00:10 +0200

    first try

---
+++ src/network_algo.erl
@@ -0,0 +1,17 @@
+%%%-------------------------------------------------------------------
+%%% @author david
+%%% @copyright (C) 2019, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 06. Mar 2019 12:12 PM
+%%%-------------------------------------------------------------------
+-module(network_algo).
+-author("david").
+
+
+
+%% API
+-export([init/0]).
+
+init()->io:fwrite("hello, world\n").
